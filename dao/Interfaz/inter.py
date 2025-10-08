09d16aec452be19793743f00dcd1529330d6d0fd
Luis David Flores <floresluisutn@gmail.com>
2025-10-08 20:31:09 -0300

    Se creo la carpeta interfaz y archivo inter

---
+++ dao/Interfaz/inter.py
@@ -0,0 +1,20 @@
+from abc import ABC , abstractmethod
+
+class DataAccessDAO(ABC):
+    @abstractmethod
+    def get(self,id : int):
+        pass
+    @abstractmethod
+    def create(self,object):
+        pass
+    
+    @abstractmethod
+    def update(self,object):
+        pass
+    
+    @abstractmethod
+    def delete(self,object):
+        pass
+    
+    
+    
